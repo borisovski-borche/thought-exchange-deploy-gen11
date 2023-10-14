81c93454a9aa3938f96aa91933e74ebfb12940a2
borisovski-borche <borisovski.borce@gmail.com>
2023-10-14 09:38:17 +0200

    changed token expiration

--- api/const/jwt.const.js
+++ api/const/jwt.const.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 
 export const createAccessToken = userId => {
   return jwt.sign({ userId }, process.env.ACCESS_TOKEN_SECRET, {
-    expiresIn: "5s",
+    expiresIn: "1m",
   });
 };
 
